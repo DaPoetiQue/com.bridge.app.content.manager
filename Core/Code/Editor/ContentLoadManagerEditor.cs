07ecf80dba0f8bdb22258d85e17b03568cbb7ca8
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2021-07-06 01:13:30 +0200

    Fix: Updated the manager editor script

--- Core/Code/Editor/ContentLoadManagerEditor.cs
+++ Core/Code/Editor/ContentLoadManagerEditor.cs
@@ -4,10 +4,10 @@ namespace Bridge.Core.App.Content.Manager
 {
     public class ContentLoadManagerEditor : Editor
     {
-        [MenuItem("Bridge/Create/Content Load Manager")]
+        [MenuItem("3ridge/Create/Content Load Manager")]
         private static void CreateContentLoadManager()
         {
-            var contentLoadManager = new UnityEngine.GameObject("_Content Load Manager");
+            var contentLoadManager = new UnityEngine.GameObject("_3ridge Content Load Manager");
             contentLoadManager.AddComponent<ContentLoadManager>();
 
             if(Selection.activeGameObject != null) contentLoadManager.transform.SetParent(Selection.activeGameObject.transform);
@@ -15,7 +15,7 @@ namespace Bridge.Core.App.Content.Manager
             UnityEngine.Debug.Log("<color=white>-->></color> <color=green> Success </color>:<color=white> A content load manager has been created successfully.</color>");
         }
 
-        [MenuItem("Bridge/Create/Content Load Manager", true)]
+        [MenuItem("3ridge/Create/Content Load Manager", true)]
         private static bool CanCreateContentLoadManager()
         {
             return FindObjectOfType<ContentLoadManager>() == null;
